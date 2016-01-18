18f28a19cf308162ccd18b5ba325dd2f7f42b699
Kevin Hoogendijk <kevinhoog@gmail.com>
2016-01-19 00:32:44 +0100

    Adds some styling to tickets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116145334) do
+ActiveRecord::Schema.define(version: 20160118224819) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -72,12 +72,12 @@ ActiveRecord::Schema.define(version: 20160116145334) do
   add_index "seats", ["row_id"], name: "index_seats_on_row_id", using: :btree
 
   create_table "tickets", force: :cascade do |t|
-    t.integer  "price",      limit: 4
-    t.string   "type",       limit: 255
-    t.boolean  "paid",       limit: 1
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "price",       limit: 4
+    t.string   "ticket_type", limit: 255
+    t.boolean  "paid",        limit: 1
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   add_index "tickets", ["user_id"], name: "index_tickets_on_user_id", using: :btree
