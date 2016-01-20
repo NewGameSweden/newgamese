4c86c78f400b6cdbefb550930a0903a8a6169329
Criticalstone <kevinhoog@gmail.com>
2016-01-21 00:06:04 +0100

    Adds admin attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118224819) do
+ActiveRecord::Schema.define(version: 20160120230432) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 20160118224819) do
     t.datetime "last_sign_in_at"
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.boolean  "admin",                  limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
