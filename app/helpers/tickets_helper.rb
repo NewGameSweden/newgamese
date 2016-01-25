c061fb14a2b62eecad987fa79e4d55d7e68cf394
Criticalstone <kevinhoog@gmail.com>
2016-01-25 12:17:50 +0100

    Adds helper to check if ticket exists

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,10 @@
 module TicketsHelper
+  def is_token_correct(token)
+    Ticket.all.each do |ticket|
+      if ticket.token.to_s == token.to_s
+        return true
+      end
+    end
+    return false
+  end
 end
