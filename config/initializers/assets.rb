7d52c76d053dcdec1540d77fbfb6daf5b066db45
Kevin Hoogendijk <kevinhoog@gmail.com>
2016-02-13 09:47:56 +0100

    Adds css files to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( seatmaps.css )
+Rails.application.config.assets.precompile += %w( tickets.css )
+Rails.application.config.assets.precompile += %w( events.css )
+Rails.application.config.assets.precompile += %w( rows.css )
+Rails.application.config.assets.precompile += %w( seats.css )
+Rails.application.config.assets.precompile += %w( pages.css )
