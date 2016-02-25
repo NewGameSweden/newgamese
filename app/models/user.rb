e282863b29438b02ce6dda7a36ec4bb145f055e0
Criticalstone <kevinhoog@gmail.com>
2016-02-25 16:15:21 +0100

    Adds validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 	validates :username, presence: true, uniqueness: true
+	validates :first_name, :surname, :email,  :address, :postal_address, :presence => true, :allow_blank => false
+	validates :social_security_number, :phone_number, :postal_code, :presence => true, :allow_blank => false, :numericality => { only_integer: true }
+	validates :sex, presence: true, allow_blank: false
 end
