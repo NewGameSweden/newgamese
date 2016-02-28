1971c8eaeade873feea54cb37cac051467d2a7a2
Criticalstone <kevinhoog@gmail.com>
2016-02-29 00:23:14 +0100

    Adds button to send user to sverok

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   root to: "articles#index"
   get ":page" => "pages#show"
   post "tickets/:id" => "tickets#activate"
+  post "users/:id/send" => "users#send_usr_to_sverok"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
