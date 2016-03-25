2681caae70ba61ce18abf3100ff5d2b3726f6f72
Criticalstone <kevinhoog@gmail.com>
2016-03-25 14:28:01 +0100

    Adds the paralax styling to articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-	
+
 	def new
 		@article = Article.new
 	end
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-  		
+
   		if @article.save
   			redirect_to @article
   		else
