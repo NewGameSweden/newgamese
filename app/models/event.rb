d93996269f5a00d22965e8f7728e704c06dbabb4
Criticalstone <kevinhoog@gmail.com>
2016-03-30 14:17:36 +0200

    Makes some cleanup in the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ActiveRecord::Base
-  has_one :seatmap
   validates :name, allow_blank: false, presence: true
   validates :description, allow_blank: false, presence: true
   validates :last_reserv_date, allow_blank: false, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
 	include Gravtastic
 	gravtastic
-	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable and :omniauthable
-	has_many :tickets
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 	validates :username, presence: true, uniqueness: true
