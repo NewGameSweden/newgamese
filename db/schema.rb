6122bc6ed0a6846ea33fdea71536970465c22f71
Criticalstone <kevinhoog@gmail.com>
2016-03-31 14:50:50 +0200

    Adds price to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330122545) do
+ActiveRecord::Schema.define(version: 20160331124409) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160330122545) do
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.string   "image",            limit: 255
+    t.integer  "price",            limit: 4
   end
 
   create_table "rows", force: :cascade do |t|
