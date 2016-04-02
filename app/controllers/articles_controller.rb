94b68dd941b3709679d1a5c51afb61198902e953
Criticalstone <kevinhoog@gmail.com>
2016-04-02 16:02:35 +0200

    Removes authentication check from index and show of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:index, :show]
+  
 	def index
 		@articles = Article.all
 	end
