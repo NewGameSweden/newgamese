d33469d326c84e921d8fc7a57dd1e95bda55ff54
Criticalstone <kevinhoog@gmail.com>
2016-04-04 21:57:19 +0200

    Moves authorize admin method to application controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,6 @@
 class AdminController < ApplicationController
   before_action :authorize_admin
-  def authorize_admin
-    redirect_to "/", alert: "Access denied" unless user_signed_in? && current_user.admin?
-  end
+
   def index
     @users=User.all
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authorize_admin
+    redirect_to "/", alert: "Access denied" unless user_signed_in? && current_user.admin?
+  end
 end
