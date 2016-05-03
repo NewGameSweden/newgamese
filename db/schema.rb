f444e9a26a048c146e8200c5d10efc6be1dac2d7
Criticalstone <kevinhoog@gmail.com>
2016-05-03 23:39:10 +0200

    Adds about page and adds styrelsen to it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417112530) do
+ActiveRecord::Schema.define(version: 20160503062222) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 20160417112530) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "about", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "rows", "seatmaps"
   add_foreign_key "seatmaps", "events"
